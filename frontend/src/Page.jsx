3514e1764837b7bd891bb5c6c4610ecb9b49ea4f
Tabish Akhtar <tabishakhtar007860@gmail.com>
2024-10-18 14:47:20 +0500

    Updated name

--- frontend/src/Page.jsx
+++ frontend/src/Page.jsx
@@ -1,42 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./Page.css";
 
 class Page extends Component {
-    render() {
-        return (
-            <div className="login">
-                <h4>Login</h4>
-                <form>
-                    <div className="text_area">
-                        <input
-                            type="text"
-                            id="username"
-                            name="username"
-                            // defaultValue="username"
-                            className="text_input"
-                            placeholder= "Your Username"
-                        />
-                    </div>
-                    <div className="text_area">
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            // defaultValue="password"
-                            className="text_input"
-                            placeholder= "Your Password"
-                        />
-                    </div>
-                    <input
-                        type="submit"
-                        value="LOGIN"
-                        className="btn"
-                    />
-                </form>
-                <a className="link" href="/signup">Sign Up</a>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="login">
+        <h4>Login page is here</h4>
+        <form>
+          <div className="text_area">
+            <input
+              type="text"
+              id="username"
+              name="username"
+              // defaultValue="username"
+              className="text_input"
+              placeholder="Your Username"
+            />
+          </div>
+          <div className="text_area">
+            <input
+              type="password"
+              id="password"
+              name="password"
+              // defaultValue="password"
+              className="text_input"
+              placeholder="Your Password"
+            />
+          </div>
+          <input type="submit" value="LOGIN" className="btn" />
+        </form>
+        <a className="link" href="/signup">
+          Sign Up
+        </a>
+      </div>
+    );
+  }
 }
 
 export default Page;
